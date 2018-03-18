4bae4dc53e48e40b8ece59a1a3460f5e2890146c
Felipe Lefèvre Marino <felipemarino91@gmail.com>
2018-03-17 23:14:27 -0300

    fastlane, slather and travis
    updated .gitignore

---
+++ fastlane/Fastfile.swift
@@ -0,0 +1,27 @@
+// This file contains the fastlane.tools configuration
+// You can find the documentation at https://docs.fastlane.tools
+//
+// For a list of all available actions, check out
+//
+//     https://docs.fastlane.tools/actions
+//
+
+import Foundation
+
+class Fastfile: LaneFile {
+    func testLane() {
+        desc("Build project tests and generate coverage report")
+        cocoapods(
+            clean: true,
+            podfile: "Podfile"
+        )
+        scan(
+            workspace: "GroceryList.xcworkspace",
+            scheme: "GroceryList"
+        )
+        slather(
+            proj: "GroceryList.xcodeproj",
+            scheme: "GroceryList"
+        )
+    }
+}
