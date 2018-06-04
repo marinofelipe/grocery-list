b1cdc45bc8e2b1e7f1880197ffb44e454d31ae60
Felipe Lefèvre Marino <felipemarino91@gmail.com>
2018-06-04 01:02:13 -0300

    ignore Pods/

--- Pods/Target Support Files/Pods-GroceryListTests/Pods-GroceryListTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_GroceryListTests : NSObject
-@end
-@implementation PodsDummy_Pods_GroceryListTests
-@end
--- Pods/Target Support Files/Pods-GroceryListUITests/Pods-GroceryListUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_GroceryListUITests : NSObject
-@end
-@implementation PodsDummy_Pods_GroceryListUITests
-@end
